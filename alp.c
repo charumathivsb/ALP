d864b7cb649a072301837dab1ff5ee18d077d776
charumathivsb <31505787+charumathivsb@users.noreply.github.com>
2018-01-18 15:41:10 +0530

    Create alp.c

---
+++ alp.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char ch;
+scanf("%c",&ch);
+if((ch >= 'a' && ch <='z')||(ch >= 'A' && ch <= 'Z'))
+{
+printf("alphabet");
+}
+else
+{
+printf("not an alphabet");
+}
+}
